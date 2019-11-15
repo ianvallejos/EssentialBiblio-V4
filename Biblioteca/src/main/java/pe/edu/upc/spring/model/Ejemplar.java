5f461b8dce1ced8f3d4f7ca3c20a49b8fe2560b9
ianvallejos <55871855+ianvallejos@users.noreply.github.com>
2019-11-15 02:05:47 -0500

    Add files via upload

---
+++ Biblioteca/src/main/java/pe/edu/upc/spring/model/Ejemplar.java
@@ -0,0 +1,70 @@
+package pe.edu.upc.spring.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+
+@Entity
+@Table(name="Ejemplar")
+
+public class Ejemplar implements Serializable {
+	
+private static final long serialVersionUID =1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idEjemplar;
+	
+	@NotEmpty(message="No puede estar vacio")
+	@NotBlank(message="No puede estar en blanco")
+	private int cantidadEjemplar;
+
+	@ManyToOne
+	@JoinColumn(name="idLibro", nullable=false)	
+	private Libro libro;
+	
+	public Ejemplar() {
+		super();
+	}
+
+	public Ejemplar(int idEjemplar,
+			int cantidadEjemplar,Libro libro) {
+		super();
+		this.idEjemplar = idEjemplar;
+		this.cantidadEjemplar = cantidadEjemplar;
+		this.libro=libro;
+	}
+
+	public int getIdEjemplar() {
+		return idEjemplar;
+	}
+
+	public void setIdEjemplar(int idEjemplar) {
+		this.idEjemplar = idEjemplar;
+	}
+
+	public int getCantidadEjemplar() {
+		return cantidadEjemplar;
+	}
+
+	public void setCantidadEjemplar(int cantidadEjemplar) {
+		this.cantidadEjemplar = cantidadEjemplar;
+	}
+
+	public Libro getLibro() {
+		return libro;
+	}
+
+	public void setLibro(Libro libro) {
+		this.libro = libro;
+	}
+}
